ea9f606ca54d8aa97a006ebdd60c11632b40c69b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-03-09 23:47:30 +0000

    added necessary files to begin project. updated structure.

--- free_edtech_tools/lib/free_edtech_tools.rb
+++ free_edtech_tools/lib/free_edtech_tools.rb
@@ -1,5 +1,13 @@
 require "free_edtech_tools/version"
 
-module FreeEdtechTools
-  # Your code goes here...
-end
+# module FreeEdtechTools
+#   # Your code goes here...
+# end
+
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative './free_edtech_tools/version.rb'
+require_relative './free_edtech_tools/edtech.rb'
+require_relative './free_edtech_tools/command_line_interface.rb'
